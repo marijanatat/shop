7c7b9198476eae632f0ee48f5eb12c079deddaed
Marijana Tatalovic <tatalovicmarijana@gmail.com>
2020-01-31 13:59:46 +0100

    'cart utils addes, grouping items in cart, quantity props'

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,28 +1,48 @@
 import React from 'react'
 import './collection-item.styles.scss'
 import {withRouter} from 'react-router-dom'
+import Button from '../button/button.component'
+import {connect} from 'react-redux'
+import {addItem} from '../../redux/cart/cart.actions'
 
-
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+// const CollectionItem = ({ id, name, price, imageUrl, history,addItem}) => (
+  const CollectionItem = ({ item,addItem}) => {
+    const { id, name, price, imageUrl, history}=item;
+    return(
   <div className='collection-item'>
     <div
       className='image'
       style={{
         backgroundImage: `url(${imageUrl})`
-      }}
-    />
+      }}>
+          {/* <div className='cart' onClick={()=>history.push('/cart-dropdown')}>
+              <div className='content'>
+                  ADD TO CART
+                </div>
+         </div> */}
+ <Button onClick={()=>addItem(item)} inverted >ADD TO CART</Button>
+     </div>
+    
+ 
+    
     <div className='collection-footer'>
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
     </div>
+    {/* <Button inverted >ADD TO CART</Button> */}
+   
   </div>
-);
+    )
+};
 
-export default withRouter(CollectionItem);;
-// const CollectionItem=({id,name,imageUrl, history, match, linkUrl})=>(
-//     <div className='collection-item' onClick={()=>history.push(`${match.url}${linkUrl}`)}>
-//         {/* <div className='collection-item'> */}
-//             <div
+const mapDispatchToProps=dispatch=>({
+  addItem: item=>dispatch(addItem(item))
+});
+
+export default connect(null,mapDispatchToProps)(CollectionItem);;
+ {/* const CollectionItem=({id,name,imageUrl, history, match, linkUrl})=>(
+     <div className='collection-item' onClick={()=>history.push(`${match.url}${linkUrl}`)}> */}
+         {/* <div className='collection-item'> */}             //<div
 //             className='background-image1'
 //             style={{
 //                 backgroundImage:`url(${imageUrl})`
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,4 +1,5 @@
 const CartActionTypes={
-    TOGGLE_CART:'TOGGLE_CART'
+    TOGGLE_CART:'TOGGLE_CART',
+    ADD_ITEM:'ADD_ITEM'
 };
 export default CartActionTypes;
